fe07b9198320651dffdb37506c61b2d8763fc2fc
pravinpoudel <prvnpoudel4@gmail.com>
2022-09-14 16:20:23 -0600

    homework1 finished

--- Assignment 1/question1.py
+++ Assignment 1/question1.py
@@ -17,23 +17,23 @@ edgeList = [(1, 6, {"id":"c"}), (1, 3, {"id":"a"}), (1, 4, {"id":"b"}), (2, 6, {
 G.add_edges_from(edgeList)
 
 # what are the degree of node 1 and 4
-print("degree of node 1 is", len(list(G.adj[1])), "and degree of node 4 is ", len(list(G.adj[4])))
+print("degree of node 1 is", len(list(G.adj[1])), "and degree of node 4 is ", len(list(G.adj[4])),  "\n")
 
 #what is the shortest path from node 3 to node 5
 shortestPath = nx.shortest_path(G, 3, 5) # compute shortest path from 3 to 5 whereas default is dijkstra
-print("shortest path from node 3 to node 4 is", shortestPath)
+print("shortest path from node 3 to node 4 is", shortestPath, "\n")
 
 #edge list of graph
 print("edge list of the graph is", G.edges)
 
 
 #adjacency list of Graph is
-print("adjacency list of graph is", G.adj)
+print("adjacency list of graph is", G.adj,  "\n")
 
 # adjacency matrix A of graph
 A = nx.to_numpy_matrix(G)  
 # we could have also got it by nx.adjacency_matrix(G).toarray()
-print("adjacency matrix of Graph is", A);
+print("adjacency matrix of Graph is \n", A,  "\n");
 
 # degree matrix 𝐷 of graph
 degreeList = []
@@ -41,24 +41,22 @@ for degree1 in G.degree():
     degreeList.append(degree1[1])
 
 D = np.diag(degreeList)
-print("degree matrix of graph is \n", D)
+print("degree matrix of graph is \n", D,  "\n")
 
 #laplacian matrix of graph is:
 L = np.subtract(D, A)
-print("Laplacian matrix of graph is ", L)
+print("Laplacian matrix of graph is \n", L,  "\n")
 
 #Relation between A, D and C
-
+print("difference between output matrix generated from product of incidence matrix and transpose of that incidence matrix (C*T(C)) and Degree Matrix (D) is equal to the adjacent matrix (A) of that graph \n")
 
 #incidence matrix of Graph is:
 I = nx.incidence_matrix(G).todense();
-print("incidence matrix is ", I)
-#  draw the line graph of 𝐺𝐺(𝐸𝐸, 𝑉𝑉)
+print("\n incidence matrix is ", I,  "\n")
+#  draw the line graph of 𝐺𝐺(𝐸, 𝑉)
 plt.figure(1)
-nx.draw_networkx(G, pos=nx.spring_layout(G, iterations = 1000),  arrows = False, with_labels = True)
-
-
-
+H= nx.line_graph(G)
+nx.draw_networkx(H, pos=nx.spring_layout(H, iterations = 1000),  arrows = False, with_labels = True)
 plt.show()
 
 
--- Assignment 1/question2.py
+++ Assignment 1/question2.py
@@ -1,4 +1,5 @@
 
+from tkinter.tix import Tree
 import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
@@ -15,37 +16,44 @@ I2 = G.in_degree(2)
 I6 = G.in_degree(6)
 O2 = G.out_degree(2)
 O6 = G.out_degree(6)
-
-print("in-degrees and out-degrees of node 2 and 6 are ", I2, ", ", I6,", ", O2,", ", O6, " respectively")
+print("in-degrees and out-degrees of node 2 and 6 are ", I2, ", ", I6,", ", O2,", ", O6, " respectively",  "\n")
 # 2. What is the shortest path from node 4 to 3?
 S = nx.shortest_path(G, 4, 3)
-print("the shortest path from node 4 to 3 is ", S)
+print("the shortest path from node 4 to 3 is ", S,  "\n")
 # 3. Is the graph a strongly connected graph?
 SC = nx.is_strongly_connected(G)
-print("is this graph strongly connected? :", SC)
+print("is this graph strongly connected? :", SC,  "\n")
 # 4. What is the largest subgraph that is strongly connected?
 largest = max(nx.strongly_connected_components(G), key=len)
-print("largest connected subgraph is", largest)
+print("largest connected subgraph is", largest,  "\n")
 # 5. Is the graph a directed acyclic graph?
 DAG = nx.is_directed_acyclic_graph(G)
-print("Is the graph a directed acyclic graph?: ", DAG)
+print("Is the graph a directed acyclic graph?: ", DAG,  "\n")
 # 6. What is the diameter of this graph?
 # Dm = nx.diameter(G)
-print("diameter of this graph is - ","since this is  not strongly connected directed network and diameter is infinite because we have nodes which has no path to other nodes ie. infinite diameter" )
+print("diameter of this graph is - ","since this is  not strongly connected directed network and diameter is infinite because we have nodes which has no path to other nodes ie. infinite diameter" ,  "\n")
 # 7. What is the edge list representation of 𝐺𝐺(𝐸𝐸, 𝑉𝑉)?
-print("edge list of network is ", list(G.edges()))
+print( "\n", "edge list of network is ", list(G.edges()))
 # 8. What is the adjacency list of 𝐺𝐺(𝐸𝐸, 𝑉𝑉)?
-print("adjacency list of network is ", G.adj)
+print( "\n", "adjacency list of network is ", G.adj,  "\n")
 # 9. What is the adjacency matrix 𝐴 of 𝐺𝐺(𝐸𝐸, 𝑉𝑉)?
 A = nx.to_numpy_matrix(G)  
-print("the adjacency matrix of graph is", A)
-# 10. What are the out-edge incidence matrix 𝐵 and the in-edge incidence matrix 𝐸 of 𝐺𝐺(𝐸, 𝑉)?
-OEI = nx.incidence_matrix(G, , )
+print("\n", "the adjacency matrix of graph is","\n", A,  "\n")
+
 # 11. What is the relationship between matrices 𝐴 , 𝐵 , and 𝐸 ?
+print("\n", "product of out edge incidence matrix and transpose of in-edge incidence matrix gives Adjacent Matrix",  "\n")
 
 # 12. Please draw the line graph of 𝐺𝐺(𝐸𝐸, 𝑉𝑉).
 plt.figure(1)
-nx.draw_networkx(G, pos=nx.spring_layout(G, iterations = 1000),  arrows = True, arrowsize=10, node_size=100, with_labels = True)
+# generating the line graph from the directed graph G
+H = nx.line_graph(G)
+nx.draw_networkx(H, arrows = True, arrowsize=10, node_size=100, with_labels = True)
 plt.show()
+
 # 13. What is the adjacency matrix 𝑀 of the line graph of 𝐺(𝐸, 𝑉)?
+AL = nx.to_numpy_matrix(H)
+print("\n", "the adjacency matrix of line graph is","\n", AL,  "\n")
+
+
 # 14. What is the relationship the matrices M , 𝐵 , and 𝐸?
+print("product of transpose of in-edge incidence Matrix(E) and out-edge incidence matrix(E) is equal to M")
